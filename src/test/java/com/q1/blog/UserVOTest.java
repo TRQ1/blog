5181c132adfc85b5227f0e65c0143415b9460574
이규원 <q1.lee@opennaru.com>
2018-08-22 22:34:12 +0900

    JUnit 테스

---
+++ src/test/java/com/q1/blog/UserVOTest.java
@@ -0,0 +1,13 @@
+package com.q1.blog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+public class UserVOTest {
+
+	@Test
+	public void printTest() {
+		System.out.println("Test");
+	}
+	
+}
