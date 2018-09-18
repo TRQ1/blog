7be3dc7d96793be1ead81c6cd2df79d13e01ccdc
이규원 <trq1.lee@gmail.com>
2018-09-18 11:23:56 +0900

    Comment, Post 컨트롤러 추가(글쓰기, 덧글만 - 수정 필요)

--- src/main/java/com/q1/blog/controller/PostController.java
+++ src/main/java/com/q1/blog/controller/PostController.java
@@ -1,34 +1,23 @@
 package com.q1.blog.controller;
 
 import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.text.DateFormat;
-import java.util.Date;
 import java.util.List;
-import java.util.Locale;
 
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
-import org.springframework.web.multipart.MultipartFile;
 
-import com.q1.blog.vo.UserVo;
 import com.q1.blog.vo.PostVo;
-
-import com.q1.blog.dao.UserDao;
 import com.q1.blog.dao.PostDao;
 
 @Controller
@@ -48,13 +37,11 @@ public class PostController {
 				, @ModelAttribute("sessionEmail") String sessionEmail
 				, Model model) {
 		
-		if ( sessionUsername.equals("") ) {
-			return "redirect:/admin/login/login";
-		}
-		
+
 		List<PostVo> postList = postDao.selectPostList();
 	
 		model.addAttribute("postList", postList);
+
 		return "admin/posts/list";
 	}
 
@@ -75,6 +62,7 @@ public class PostController {
 		  postVo.setId(id);
 		  postVo.setUserId(userId);
 		  postVo.setTitle(title);
+		  postVo.setContent(content);
 		  
 	      postDao.insertPost(postVo);
 	      
